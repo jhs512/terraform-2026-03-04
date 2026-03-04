f76b7136a7038a9056cc322e61b369b309071c58
jhs512 <jangka512@gmail.com>
2026-03-04 12:20:48 +0900

    0023

--- main.tf
+++ main.tf
@@ -79,6 +79,28 @@ resource "aws_subnet" "subnet_2" {
 }
 
 
+// AWS 서브넷 리소스를 생성하고 이름을 'subnet_3'로 설정
+resource "aws_subnet" "subnet_3" {
+
+  // 이 서브넷이 속할 VPC를 지정. 여기서는 'vpc_1'를 선택
+  vpc_id = aws_vpc.vpc_1.id
+
+  // 서브넷의 IP 주소 범위를 설정
+  cidr_block = "10.0.3.0/24"
+
+  // 서브넷이 위치할 가용 영역을 설정
+  availability_zone = "${var.region}c"
+
+  // 이 서브넷에 배포되는 인스턴스에 공용 IP를 자동으로 할당
+  map_public_ip_on_launch = true
+
+  // 리소스에 대한 태그를 설정
+  tags = {
+    Name = "${var.prefix}-subnet-3"
+  }
+}
+
+
 // AWS 인터넷 게이트웨이 리소스를 생성하고 이름을 'igw_1'로 설정
 resource "aws_internet_gateway" "igw_1" {
 
@@ -133,6 +155,17 @@ resource "aws_route_table_association" "association_2" {
 }
 
 
+// 라우트 테이블 'rt_1'과 서브넷 'subnet_3'을 연결
+resource "aws_route_table_association" "association_3" {
+
+  // 연결할 서브넷을 지정
+  subnet_id = aws_subnet.subnet_3.id
+
+  // 연결할 라우트 테이블을 지정
+  route_table_id = aws_route_table.rt_1.id
+}
+
+
 // AWS 보안 그룹 리소스를 생성하고 이름을 'sg_1'로 설정
 resource "aws_security_group" "sg_1" {
 
@@ -169,5 +202,107 @@ resource "aws_security_group" "sg_1" {
   // 리소스에 대한 태그를 설정
   tags = {
     Name = "${var.prefix}-sg-1"
+  }
+}
+
+# EC2 역할 생성
+resource "aws_iam_role" "ec2_role_1" {
+  name = "${var.prefix}-ec2-role-1"
+
+  # 이 역할에 대한 신뢰 정책 설정. EC2 서비스가 이 역할을 가정(사용)할 수 있도록 설정
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+      }
+    ]
+  })
+}
+
+
+# EC2 역할에 AmazonS3FullAccess 정책을 부착
+resource "aws_iam_role_policy_attachment" "s3_full_access" {
+  role       = aws_iam_role.ec2_role_1.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+
+# EC2 역할에 최신 SSM 정책을 부착
+resource "aws_iam_role_policy_attachment" "ec2_ssm" {
+  role       = aws_iam_role.ec2_role_1.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
+# IAM 인스턴스 프로파일 생성
+resource "aws_iam_instance_profile" "instance_profile_1" {
+  name = "${var.prefix}-instance-profile-1"
+  role = aws_iam_role.ec2_role_1.name
+}
+
+
+# 최신 Amazon Linux AMI 조회
+data "aws_ssm_parameter" "amazon_linux_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+
+# EC2 인스턴스 생성
+resource "aws_instance" "ec2_1" {
+
+  # 사용할 AMI ID
+  ami = data.aws_ssm_parameter.amazon_linux_ami.value
+
+  # EC2 인스턴스 유형
+  instance_type = "t3.micro"
+
+  # 사용할 서브넷 ID
+  subnet_id = aws_subnet.subnet_1.id
+
+  # 적용할 보안 그룹 ID
+  vpc_security_group_ids = [aws_security_group.sg_1.id]
+
+  # 퍼블릭 IP 연결 설정
+  associate_public_ip_address = true
+
+  # 인스턴스에 IAM 역할 연결
+  iam_instance_profile = aws_iam_instance_profile.instance_profile_1.name
+
+  # 인스턴스에 태그 설정
+  tags = {
+    Name = "${var.prefix}-ec2-1"
+  }
+}
+
+
+# EC2 인스턴스 생성
+resource "aws_instance" "ec2_2" {
+
+  # 사용할 AMI ID
+  ami = data.aws_ssm_parameter.amazon_linux_ami.value
+
+  # EC2 인스턴스 유형
+  instance_type = "t3.micro"
+
+  # 사용할 서브넷 ID
+  subnet_id = aws_subnet.subnet_3.id
+
+  # 적용할 보안 그룹 ID
+  vpc_security_group_ids = [aws_security_group.sg_1.id]
+
+  # 퍼블릭 IP 연결 설정
+  associate_public_ip_address = true
+
+  # 인스턴스에 IAM 역할 연결
+  iam_instance_profile = aws_iam_instance_profile.instance_profile_1.name
+
+  # 인스턴스에 태그 설정
+  tags = {
+    Name = "${var.prefix}-ec2-2"
   }
 }
